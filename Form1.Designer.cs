c35f81558d3dcef134d7058b029eb4b648fad3bb
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2022-07-21 09:41:16 +0800

    not so important additions

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,152 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mainFormElipse = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.pnlTitleBar = new System.Windows.Forms.Panel();
+            this.btnExport = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnOpen = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlTitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainFormElipse
+            // 
+            this.mainFormElipse.ElipseRadius = 15;
+            this.mainFormElipse.TargetControl = this;
+            // 
+            // pnlTitleBar
+            // 
+            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(122)))), ((int)(((byte)(235)))));
+            this.pnlTitleBar.Controls.Add(this.label1);
+            this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitleBar.Name = "pnlTitleBar";
+            this.pnlTitleBar.Size = new System.Drawing.Size(1280, 75);
+            this.pnlTitleBar.TabIndex = 0;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Active = false;
+            this.btnExport.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(122)))), ((int)(((byte)(235)))));
+            this.btnExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(122)))), ((int)(((byte)(235)))));
+            this.btnExport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExport.BorderRadius = 0;
+            this.btnExport.ButtonText = "export";
+            this.btnExport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExport.DisabledColor = System.Drawing.Color.Gray;
+            this.btnExport.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnExport.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnExport.Iconimage")));
+            this.btnExport.Iconimage_right = null;
+            this.btnExport.Iconimage_right_Selected = null;
+            this.btnExport.Iconimage_Selected = null;
+            this.btnExport.IconMarginLeft = 0;
+            this.btnExport.IconMarginRight = 0;
+            this.btnExport.IconRightVisible = true;
+            this.btnExport.IconRightZoom = 0D;
+            this.btnExport.IconVisible = true;
+            this.btnExport.IconZoom = 90D;
+            this.btnExport.IsTab = false;
+            this.btnExport.Location = new System.Drawing.Point(1101, 660);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(122)))), ((int)(((byte)(235)))));
+            this.btnExport.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(102)))), ((int)(((byte)(237)))));
+            this.btnExport.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnExport.selected = false;
+            this.btnExport.Size = new System.Drawing.Size(167, 48);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "export";
+            this.btnExport.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExport.Textcolor = System.Drawing.Color.White;
+            this.btnExport.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Active = false;
+            this.btnOpen.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(122)))), ((int)(((byte)(235)))));
+            this.btnOpen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(122)))), ((int)(((byte)(235)))));
+            this.btnOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnOpen.BorderRadius = 0;
+            this.btnOpen.ButtonText = "open";
+            this.btnOpen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOpen.DisabledColor = System.Drawing.Color.Gray;
+            this.btnOpen.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnOpen.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnOpen.Iconimage")));
+            this.btnOpen.Iconimage_right = null;
+            this.btnOpen.Iconimage_right_Selected = null;
+            this.btnOpen.Iconimage_Selected = null;
+            this.btnOpen.IconMarginLeft = 0;
+            this.btnOpen.IconMarginRight = 0;
+            this.btnOpen.IconRightVisible = true;
+            this.btnOpen.IconRightZoom = 0D;
+            this.btnOpen.IconVisible = true;
+            this.btnOpen.IconZoom = 90D;
+            this.btnOpen.IsTab = false;
+            this.btnOpen.Location = new System.Drawing.Point(928, 660);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(122)))), ((int)(((byte)(235)))));
+            this.btnOpen.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(102)))), ((int)(((byte)(237)))));
+            this.btnOpen.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnOpen.selected = false;
+            this.btnOpen.Size = new System.Drawing.Size(167, 48);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "open";
+            this.btnOpen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpen.Textcolor = System.Drawing.Color.White;
+            this.btnOpen.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SAMPLE";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(859, 577);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.pnlTitleBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.pnlTitleBar.ResumeLayout(false);
+            this.pnlTitleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse mainFormElipse;
+        private System.Windows.Forms.Panel pnlTitleBar;
+        private Bunifu.Framework.UI.BunifuFlatButton btnExport;
+        private Bunifu.Framework.UI.BunifuFlatButton btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,25 @@ namespace HFRM_SALARY_CALCULATOR
 {
     public partial class Form1 : Form
     {
+
+        OpenFileDialog ofd = new OpenFileDialog();
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose an excel file";
+            ofd.InitialDirectory = @"C:\";
+            ofd.Filter = "Excel Sheet(*.xls)|*.xls|All Files(*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+            }
         }
     }
 }
